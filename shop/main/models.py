1c406cc909458089b94211f489438fcc9838ec0e
Alexander <privetworld@gmail.com>
2024-04-18 00:00:38 +0300

    added image in models

--- shop/main/models.py
+++ shop/main/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100, unique=True, null=False, blank=False, verbose_name= 'Наименование')
     price = models.IntegerField(verbose_name='Цена')
     description = models.TextField(max_length=300, verbose_name='Описание')
+    image = models.ImageField(blank=True, upload_to='images')
 
     class Meta:
         verbose_name = 'Продукт'
--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -1,5 +1,7 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 
 urlpatterns = [
@@ -8,3 +10,6 @@ urlpatterns = [
     # http://127.0.0.1:8000/
     path('', include('main.urls', namespace='main')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
